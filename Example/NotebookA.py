54ec6df6464569d7ad6885dff267ec44f5de7d8a
Miguel-Silva-IST <Miguel-Silva-IST>
2022-11-11 11:34:49 +0000

    sim

---
+++ Example/NotebookA.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+a=7
+b=5
+
+# COMMAND ----------
+
+c=a+b
+
+# COMMAND ----------
+
+# MAGIC %run ./NotebookB
+
+# COMMAND ----------
+
+
--- Example/NotebookB.py
+++ Example/NotebookB.py
@@ -1,2 +1,2 @@
 # Databricks notebook source
-a=1
+print(c)
